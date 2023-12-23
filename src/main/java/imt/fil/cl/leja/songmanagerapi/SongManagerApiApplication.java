a03215f09f8305ea8bb47157d2e8a870dba88f4a
Soufiane EZZEMANY <soufianezzemany@gmail.com>
2023-12-23 13:17:43 +0100

    init project

---
+++ src/main/java/imt/fil/cl/leja/songmanagerapi/SongManagerApiApplication.java
@@ -0,0 +1,13 @@
+package imt.fil.cl.leja.songmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongManagerApiApplication.class, args);
+    }
+
+}
